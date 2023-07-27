e0d6ede3d9ab343259a240c826465f9af2d39753
Priyankajadkar <140548319+Priyankajadkar@users.noreply.github.com>
2023-07-27 17:16:54 +0530

    Resolve the Api

--- weather_app.py
+++ weather_app.py
@@ -1,55 +1,75 @@
 import requests
 
-API_BASE_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly"  # Replace with the correct API URL
+API_BASE_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22"
 
-def get_weather_data(date):
-    url = f"{API_BASE_URL}?date={date}"
-    response = requests.get(url)
+def get_weather_data():
+    response = requests.get(API_BASE_URL)
     if response.status_code == 200:
         return response.json()
     else:
         print("Error fetching weather data.")
         return None
 
-def get_weather_temperature():
-    date = input("Enter the date (YYYY-MM-DD): ")
-    weather_data = get_weather_data(date)
-    if weather_data:
-        print(weather_data)  # Print the entire API response to inspect the data
+def get_temperature_by_date(data, date):
+    for item in data['list']:
+        if item['dt_txt'] == date:
+            return item['main']['temp']
+    return None
 
-        # Print all keys in the response
-        print("Available keys:", weather_data.keys())
+def get_wind_speed_by_date(data, date):
+    for item in data['list']:
+        if item['dt_txt'] == date:
+            return item['wind']['speed']
+    return None
 
-        # Find the correct key for temperature and update the print statement accordingly
-        # For example, if the key for temperature is 'main'['temp'], modify the line below:
-        # print(f"Temperature on {date}: {weather_data['main']['temp']} °C")
-
-def get_wind_speed():
-    date = input("Enter the date (YYYY-MM-DD): ")
-    weather_data = get_weather_data(date)
-    if weather_data:
-        print(f"Wind Speed on {date}: {weather_data['wind']['speed']} m/s")
-
-def get_pressure():
-    date = input("Enter the date (YYYY-MM-DD): ")
-    weather_data = get_weather_data(date)
-    if weather_data:
-        print(f"Pressure on {date}: {weather_data['pressure']} hPa")
+def get_pressure_by_date(data, date):
+    for item in data['list']:
+        if item['dt_txt'] == date:
+            return item['main']['pressure']
+    return None
 
 def main():
-    while True:
-        print("1. Get weather\n2. Get Wind Speed\n3. Get Pressure\n0. Exit")
-        choice = input("Enter your choice: ")
+    data = get_weather_data()
+    if not data:
+        return
 
-        if choice == "1":
-            get_weather_temperature()
-        elif choice == "2":
-            get_wind_speed()
-        elif choice == "3":
-            get_pressure()
-        elif choice == "0":
-            print("Exiting the program.")
+    while True:
+        print("\nMenu:")
+        print("1. Get weather temperature for a date")
+        print("2. Get wind speed for a date")
+        print("3. Get pressure for a date")
+        print("0. Exit")
+
+        choice = input("Enter your choice (0/1/2/3): ")
+
+        if choice == '1':
+            date = input("Enter the date (e.g., 2023-07-25 12:00:00): ")
+            temperature = get_temperature_by_date(data, date)
+            if temperature is not None:
+                print(f"Temperature at {date}: {temperature}°C")
+            else:
+                print("Data not available for the given date.")
+
+        elif choice == '2':
+            date = input("Enter the date (e.g., 2023-07-25 12:00:00): ")
+            wind_speed = get_wind_speed_by_date(data, date)
+            if wind_speed is not None:
+                print(f"Wind Speed at {date}: {wind_speed} m/s")
+            else:
+                print("Data not available for the given date.")
+
+        elif choice == '3':
+            date = input("Enter the date (e.g., 2023-07-25 12:00:00): ")
+            pressure = get_pressure_by_date(data, date)
+            if pressure is not None:
+                print(f"Pressure at {date}: {pressure} hPa")
+            else:
+                print("Data not available for the given date.")
+
+        elif choice == '0':
+            print("Exiting program.")
             break
+
         else:
             print("Invalid choice. Please try again.")
 
